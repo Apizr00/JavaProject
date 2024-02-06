fce4621bf697244d9b628c1bb24338119b780f21
Apizr00 <hafizrodzli25@gmail.com>
2024-02-06 10:56:48 +0800

    Four Commit

--- src/App.java
+++ src/App.java
@@ -1,15 +1,28 @@
+import lesson1.IntegerHandler;
 import lesson1.StringHandler;
 
 public class App {
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
+        lesson2();
     }
 
-    public void lesson1() {
-        StringHandler stringHandler = new StringHandler();
+    public static void lesson2() {
+
+        int numberOfVessels = 15;
+        int[] dimension = { 2, 4, 7 };
+        IntegerHandler integerHandler = new IntegerHandler(numberOfVessels, dimension);
+        int volume = integerHandler.getTotalVolume();
+        String msg = integerHandler.toString();
+        System.out.println(msg);
+        System.out.println(volume);
+    }
+
+    public static void lesson1() {
+
+        StringHandler stringHandler = new StringHandler("my class");
         System.out.println(stringHandler.toString());
     }
 
-    
 }
--- src/lesson1/StringHandler.java
+++ src/lesson1/StringHandler.java
@@ -2,10 +2,28 @@ package lesson1;
 
 public class StringHandler {
 
+    private String str;
+
+    public StringHandler(String str) {
+
+        this.str = str;
+    }
+
+    public String getStr() {
+
+        return str;
+    }
+
+    public void setStr(String str) {
+
+        this.str = str;
+
+    }
+
     @Override
     public String toString() {
-
-        return "String Handler";
+        
+        return "String Handler(str:" + str + ")";
     }
 
 }
