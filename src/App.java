d9f03b853b23f3c079d9c68540c657219191982f
Apizr00 <hafizrodzli25@gmail.com>
2024-02-06 10:25:35 +0800

    Second commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import lesson1.StringHandler;
+
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+
         System.out.println("Hello, World!");
+    }
+
+    public void lesson1() {
+        StringHandler stringHandler = new StringHandler();
+        System.out.println(stringHandler.toString());
     }
 }
---
+++ src/lesson1/StringHandler.java
@@ -0,0 +1,11 @@
+package lesson1;
+
+public class StringHandler {
+
+    @Override
+    public String toString() {
+
+        return "String Handler";
+    }
+
+}
