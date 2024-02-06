b08327a8d6a212e8d82c081e848d0367a4110d59
Apizr00 <hafizrodzli25@gmail.com>
2024-02-06 14:14:32 +0800

    Six Commit

---
+++ src/lesson1/quiz1/question7.java
@@ -0,0 +1,13 @@
+package lesson1.quiz1;
+
+import java.time.LocalDate;
+
+public class question7 {
+
+    public static void main(String[] args) {
+
+        LocalDate date = LocalDate.parse("1983-06-30");
+        System.out.println(date.plusMonths(8));
+    }
+
+}
