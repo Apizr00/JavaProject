28c683adb8897ce1748bd13108e80e2c010df598
Apizr00 <hafizrodzli25@gmail.com>
2024-02-06 14:49:02 +0800

    Eight commit

---
+++ src/lesson1/quiz1/Q2/Test.java
@@ -0,0 +1,17 @@
+package lesson1.quiz1.Q2;
+
+public class Test {
+
+    static int i1 = 10;
+    int i2 = 20;
+
+    int add() {
+
+        return this.i1 + this.i2;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(new Test().add());
+    }
+}
