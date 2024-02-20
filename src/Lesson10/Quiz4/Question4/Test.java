0700bf11b78fbb5cca497a4b7e0e813c3d438694
Apizr00 <hafizrodzli25@gmail.com>
2024-02-20 15:56:17 +0800

    24 Commit

---
+++ src/Lesson10/Quiz4/Question4/Test.java
@@ -0,0 +1,12 @@
+package Lesson10.Quiz4.Question4;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("B");
+        sb.append(sb.append("A"));
+        System.out.println(sb);
+    }
+
+}
---
+++ src/Lesson10/Quiz4/Question6/Test.java
@@ -0,0 +1,12 @@
+package Lesson10.Quiz4.Question6;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        boolean status = true;
+        // System.out.println(status = false || status = true | status = false);
+        System.out.println(status);
+    }
+
+}
